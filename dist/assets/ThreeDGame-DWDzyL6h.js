e6aa6b6131a46fc31d775407b98bd17dd500e57f
Sbeft <sethpietrowsk1@gmail.com>
2025-08-25 20:44:33 -0400

    Oops, I should probably include the build files (dist)

---
+++ dist/assets/ThreeDGame-DWDzyL6h.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-CYfQAj5N.js";function t(){return e.jsx("div",{children:"You have lost game privelages."})}export{t as default};
+//# sourceMappingURL=ThreeDGame-DWDzyL6h.js.map
