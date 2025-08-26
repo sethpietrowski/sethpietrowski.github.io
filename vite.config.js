15702761fbaf308ad12c6b3c8729aac920290ac6
Sbeft <sethpietrowsk1@gmail.com>
2025-08-25 20:27:48 -0400

    Trailing slash caused 404 error in production build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/sethpietrowski.github.io/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/sethpietrowski.github.io' : '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
