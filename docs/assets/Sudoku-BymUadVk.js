cb4e9845f2b06e9199b3dd70664939bb52c39b09
Sbeft <sethpietrowsk1@gmail.com>
2025-09-21 22:05:29 -0400

    Adding back in 3d model and game. Style conflicts and activate preference are still pending fix

--- docs/assets/Sudoku-BymUadVk.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./index-CdavgHE5.js";function u(){return e.jsx("div",{children:"You have lost Sudoku priveleges."})}export{u as default};
-//# sourceMappingURL=Sudoku-BymUadVk.js.map
--- docs/assets/ThreeDGame-DF-8yfNY.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./index-CdavgHE5.js";function t(){return e.jsx("div",{children:"You have lost game priveleges."})}export{t as default};
-//# sourceMappingURL=ThreeDGame-DF-8yfNY.js.map
